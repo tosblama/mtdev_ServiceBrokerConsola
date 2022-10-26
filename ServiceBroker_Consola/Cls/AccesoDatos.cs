d0a1880dca2fd1c42e10748fa00c050a57b00a7b
Manuel Toscano Blancas <tosblama@gmail.com>
2022-10-26 18:05:40 +0200

    Agregar archivos de proyecto.

---
+++ ServiceBroker_Consola/Cls/AccesoDatos.cs
@@ -0,0 +1,67 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace ServiceBroker_Consola.Cls
+{
+  internal static class AccesoDatos
+  {
+    // Cadena de conexión a la BBDD SQL
+    public static string cadenaConexion = @"Data Source=TOSBLAMA-L5\SQLSERVER;Initial Catalog=EPV;Integrated Security=true;";
+
+    /// <summary>
+    /// Ejecuta una consulta SQL en la base de datos, y devuelve los resultados obtenidos en un objeto DataTable.
+    /// </summary>
+    /// <param name="SQL"></param>
+    /// <param name="parametros">Array de string con formato: nombre:valor</param>
+    /// <returns>Devuelve un objeto DataTable con los resultados obtenidos tras ejecución de la consulta.</returns>
+    public static DataTable GetDataTable(string SQL, string[] parametros)
+    {
+      try
+      {
+        SqlConnection conexion = new SqlConnection(cadenaConexion);
+        SqlCommand comando = new SqlCommand(SQL, conexion);
+        for (int i = 0; i < parametros.Length; i++)
+          comando.Parameters.Add(new SqlParameter(parametros[i].Split(':')[0], parametros[i].Split(':')[1]));
+        SqlDataAdapter da = new SqlDataAdapter(comando);
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+        conexion.Close();
+        da.Dispose();
+        conexion.Dispose();
+        return ds.Tables[0];
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+    }
+
+
+    /// <summary>
+    /// Ejecuta una consulta SQL en la base de datos, y devuelve los resultados obtenidos en un objeto DataTable.
+    /// A diferencia de GetDataTable, este método es vulnerable a la inyección de dependencias, por lo que se recomienda usar sólo para procesos temporales internos.
+    /// </summary>
+    /// <param name="SQL"></param>
+    /// <returns>Devuelve un DataTable con los resultados de la ejecución de la consulta.</returns>
+    public static DataTable GetTmpDataTable(string SQL)
+    {
+      try
+      {
+        SqlConnection conexion = new SqlConnection(cadenaConexion);
+        SqlDataAdapter comando = new SqlDataAdapter(SQL, conexion);
+        DataSet ds = new DataSet();
+        comando.Fill(ds);
+        conexion.Close();
+        comando.Dispose(); conexion.Dispose();
+        return ds.Tables[0];
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+    }
+
+
+
+  }
+}
---
+++ ServiceBroker_Consola/Program.cs
@@ -0,0 +1,36 @@
+﻿// Ejemplo que muestra como usa ServiceBroker de SQL Server.
+// Puede utilizarse en un servicio para disparar procesos automáticos (por ejemplo crear una cola de tareas que genere PDFs, envie email, etc)
+// También se puede usar para lanzar notificaciones por WebSocket (basado en tablas)
+
+
+// Usamos nuestra clase de ServiceBroker
+using ServiceBroker_Consola.Cls;
+
+// Cadena de conexión (recomendable sacar de fichero de configuración)
+AccesoDatos.cadenaConexion = @"Data Source=TOSBLAMA-L5\SQLSERVER;Initial Catalog=EPV;Integrated Security=true;MultipleActiveResultSets=true;";
+
+// Instanciamos e inicializamos nuestra clase (El Id de usuario fijo es sólo a efectos informativos, en aplicación real sacarlo del usuario logado)
+var sb = new ServiceBrokerSQL(AccesoDatos.cadenaConexion, "SELECT IdAlerta FROM [dbo].[Alertas] WHERE IdUsuario=1", "ESCUCHAR_NUMERO_ALERTAS");
+sb.OnMensajeRecibido += new ServiceBrokerSQL.MensajeRecibido(sbColaTareas_InformacionRecibida);
+sb.IniciarEscucha();
+
+Console.Clear();
+Console.WriteLine("Esperando instrucciones");
+Thread.Sleep(Timeout.Infinite);
+sb.DetenerEscucha();
+
+
+// Controlador del evento MensajeRecibido de ServiceBrokerSQL. El parámetro nombreMensaje se usa a modo de demostración de como devolver parámetros en el evento.
+void sbColaTareas_InformacionRecibida(object sender, string nombreMensaje)
+{
+  // Procesamos el mensaje recibido
+  System.Data.DataTable dt = AccesoDatos.GetTmpDataTable("SELECT COUNT(IdAlerta) as NumeroAlertas FROM dbo.Alertas WHERE IdUsuario=1");
+  int numeroAlertas = Convert.ToInt32(dt.Rows[0]["NumeroAlertas"]);
+
+  Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} Ha llegado un cambio en {nombreMensaje}, ahora tienes {numeroAlertas} alertas");
+
+  // Volvemos a inicializar la escucha
+  sb.IniciarEscucha();
+}
+
+
